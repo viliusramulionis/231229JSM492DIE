866d790848b87e7e0d85f4fc6b60ed64e9da9dc7
vilius.ramulionis <vilius.ramulionis@gmail.com>
2024-03-12 11:40:11 +0200

    express

--- 2024-03-12/index.js
+++ 2024-03-12/index.js
@@ -2,6 +2,11 @@ import express from 'express';
 // Express karkaso iniciavimas
 const app = express();
 
+// NORINT PRIIMTI DUOMENIS URLENCODED FORMATU REGISTRUOJAMA EILUTĖ
+app.use(express.urlencoded({
+    extended: true
+}));
+
 // Kelio registravimas (Route)
 // CRUD:
 // CREATE - POST
@@ -10,20 +15,57 @@ const app = express();
 // DELETE - DELETE
 
 // galimi metodai:
-// .get() - GET HTTP metodas
+// .get() - GET HTTP metodas (GET metodu pateikiant užklausą body duomenys nėra persiunčiami)
 // .post() - POST HTTP metodas
 // .put() - PUT HTTP metodas
 // .delete() - DELETE HTTP metodas
 
-// Pirmas parametras: kelias (route)
+// Pirmas parametras: kelias (route), endpoint
 // Antras parametras: funkcija kuri bus iššaukiama vartotojui atėjus nurodytu keliu
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     // Į funkciją priimami du parametrai:
     // req - Request (Užklausa)
     // res - Response (Atsakymas)
     console.log('Funkcija aktyvuota');
     // Teksto 'Hello World' grąžinimas vartotojui (client)
-    res.send('Titulinis puslapis pakeistas');
+    res.send('<h1>Titulinis puslapis pakeistas</h1>');
+});
+
+app.get('/about', (req, res) => {
+    res.send('<h1>Apie Mus</h1>');
+});
+
+// Query parametrų prėmimas
+app.get('/query', (req, res) => {
+    // req.query - Query parametrai nugula req objekte su query raktažodžiu 
+    // console.log(req.query);
+    res.send(`
+        <li>Daina: ${req.query.song}</li>
+    `);
+});
+
+// POST http metodu perduodamų duomenų priėmimas
+app.post('/post', (req, res) => {
+    // req.body grąžina body lygmenyje persiųstus duomenis
+    console.log(req.body);
+
+    if(req.body.song === 'Live forever') {
+        console.log('Kondicija teigiama');
+    }
+
+    res.send(`
+        <li>Filmas: ${req.body.movie ? req.body.movie : 'Nerasta'}</li>
+        <li>Daina: ${req.body.song ? req.body.song : 'Nerasta'}</li>
+    `);
+});
+
+// PERDUOTŲ DUOMENŲ TIKRINIMAS
+app.post('/check', (req, res) => {
+    if(req.body.name === 'Laimis') {
+        res.send('Administratorius');
+    } else {
+        res.send('Vartotojas');
+    }
 });
 
 // Serverio paleidimas port'e 3001
